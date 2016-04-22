3d0570aa38a95ed1a2e59dba0c4ff1d15d6f12bf
Win <winnoh@gmail.com>
2016-04-21 21:37:10 -0700

    Checkpoint 1 end of day hack day 1

---
+++ Assets/My Assets/Scripts/PhoneSelect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseOver (GameObject gameObject) {
+		// Show mouse over UI element (unhide?)
+		gameObject.SetActive(true);
+	}
+
+	public void OnMouseExit (GameObject gameObject) { 
+		// Undo Mouseover
+		gameObject.SetActive(false);
+	}
+
+	public void OnMouseClick (GameObject gameObject) {
+		
+	}
+}
+
